b00df37021a1eec3a1716acbcfe22c28cce4154f
Tilma Rodriguez <carlosjavi04@gmail.com>
2022-04-02 18:58:15 -0500

    login_required, login_url, login y autheticate

--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
 
-
 def login_view(request):
-    print('que rompiiiiiiiiiiiiiiiiiiiii')
+    if request.method == 'POST':
+        user = authenticate(request, username =request.POST.get('username'), password = request.POST.get('password'))
+        if user:
+            login(request,user)
+            return redirect('list_posts')
+        else:
+            return render(request, 'users/login.html', {'error':'Clave invalidad o usuario'})
     return render(request, 'users/login.html')
--- yakigram/settings.py
+++ yakigram/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+LOGIN_URL = 'login_view'
+
 try:
     from .local_settings import *
 except:
