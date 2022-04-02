54b049a35f34a54245b23ad1e05448067eab5082
Tilma Rodriguez <carlosjavi04@gmail.com>
2022-04-02 15:58:58 -0500

    templates agregados

--- yakigram/urls.py
+++ yakigram/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', views.list_posts, name= 'list_posts')
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
   
