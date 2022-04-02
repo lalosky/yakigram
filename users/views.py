7dd41d23b330b8c2438ef022c661f45707802837
Tilma Rodriguez <carlosjavi04@gmail.com>
2022-04-02 17:02:26 -0500

    vista del login mas template login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login_view(request):
+    print('que rompiiiiiiiiiiiiiiiiiiiii')
+    return render(request, 'users/login.html')
--- yakigram/urls.py
+++ yakigram/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from posts import views
+from posts import views as posts_views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', views.list_posts, name= 'list_posts')
+    path('posts/', posts_views.list_posts, name= 'list_posts'),
+    path('users/login/', users_views.login_view, name= 'login_view')
+
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
